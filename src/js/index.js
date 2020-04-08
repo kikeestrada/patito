7e5f4a5de3adaddff8bfd1e0b0264b32ef2d1844
kikeestrada <kikeestrada@gmail.com>
2020-04-08 02:35:48 -0600

    ajustes para el portafolio

--- src/js/index.js
+++ src/js/index.js
@@ -1,24 +1,13 @@
 import {topNav} from './modules/topNav'
-import {marvelApi} from './modules/marvelApi'
 import {searchFilter} from './modules/searchFilter'
-import {lightBox} from './modules/lightBox'
-import {tabs} from './modules/tabs'
-import {edModal} from './modules/modal'
-import {activeMenuItem} from "./modules/verticalMenu";
-import {btnMenu} from "./modules/btnMenu"
 (()=>{
 	topNav();
-	lightBox();
-	tabs();
-	edModal();
-	btnMenu()
 
 	if (document.body.classList.contains('home')) {
 		// functions here
 	}else if (document.body.classList.contains('page2')) {
 		// functions here
 		searchFilter();
-		marvelApi();
 	}else if (document.body.classList.contains('page3')) {
 		// functions here
 
---
+++ src/pug/md/education.md
@@ -0,0 +1,5 @@
+## Education
+***Cenfotec***: Currently in software engineering career, Cenfotec University, San José, Costa Rica
+
+***404 Digital Academy***: Front End Design, Front End Developer, JavaScript Advanced
+                           (ES6), Digital Marketing, SEO and UX and UI Design.
